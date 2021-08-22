314931305f0419ee3bd517b39bde08e605a64c9d
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-21 21:05:42 -0300

    Item details css

--- src/Components/CountryItem/CountryItem.jsx
+++ src/Components/CountryItem/CountryItem.jsx
@@ -6,7 +6,7 @@ const CountryItem = ({name, flag, population, continent}) => {
         <div className="CountryItemContainer">
             <h2>{name} </h2>
             <div className="CountryItemDetails">
-                <img className="ItemDetail" src={flag} alt="imagen" />
+               <div className="ImageContainer"><img className="ItemDetail" src={flag} alt="imagen" /> </div> 
                 <h3 className="ItemDetail">Region: {continent} </h3>
                 <h3 className="ItemDetail">Population: {population} </h3>
             </div>

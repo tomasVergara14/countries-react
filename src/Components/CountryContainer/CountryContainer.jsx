c3163abac38c0573c93087ede7f1681151ea7fb3
tomasVergara14 <tomasvergara272@gmail.com>
2021-08-22 03:47:44 -0300

    countrySearch and countryContainer

--- src/Components/CountryContainer/CountryContainer.jsx
+++ src/Components/CountryContainer/CountryContainer.jsx
@@ -6,18 +6,17 @@ import { useSearchContext } from '../../Handlers/Context/SearchContext'
 const CountryContainer = () => {
 
     const [loading]= useState(true)
-    const {country, LoadData, countryResult }= useSearchContext()
-    console.log(countryResult)
+    const {LoadData, countryResult }= useSearchContext()
 
     useEffect(()=>{
         LoadData()
     },[])
 
-    if(country.length !==0){
+    if(countryResult.length !== 0){
         return(
             <div className="CountryContainer">
                 <div className="CountryMap">
-                    {country.map((element,index)=>{
+                    {countryResult.map((element,index)=>{
                         if(index<2){
                             return(
                                 <CountryItem key={index} name={element.name} flag={element.flag} population={element.population} continent={element.region} />
@@ -29,7 +28,10 @@ const CountryContainer = () => {
         )
     }else{
         return(
-            <div> {loading && <div className="spinner"></div>} </div>
+            <div> 
+                {loading && <div className="spinner"></div>} 
+                <h3>Try again</h3>    
+            </div>
         )
     }
     

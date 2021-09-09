06baa2df6199499c7fb2f888d384e92e60d63118
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-09 12:33:47 -0300

    sidebar responsive

---
+++ src/Components/CountryList/CountryList/CountryList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryList = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CountryList
---
+++ src/Components/CountryList/CountryListContainer/CountryListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountryListContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CountryListContainer

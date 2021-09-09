f0f4ddc3a64323ec05093e6f1b50b5e363d0885e
tomasVergara14 <tomasvergara272@gmail.com>
2021-09-09 16:12:42 -0300

    Country list css

--- src/Components/CountryList/CountryListContainer.jsx
+++ src/Components/CountryList/CountryListContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useSearchContext } from '../../Handlers/Context/SearchContext'
 import { NavLink } from 'react-router-dom'
-import CountryItem from '../CountryItem/CountryItem'
+import './CountryListContainer.css'
 
 const CountryListContainer = () => {
 
@@ -20,8 +20,8 @@ const CountryListContainer = () => {
                 <h3>Names of the Countries</h3>
                 {country.map((element, index)=>{
                    return(
-                       <div key={index}>
-                           <NavLink  to={`/detail/${element.name}`}  >{element.name}</NavLink>
+                       <div key={index} className="CountryLinksContainer">
+                           <NavLink className="CountryLink"  to={`/detail/${element.name}`}  >{element.name}</NavLink>
                        </div>
                        
                    )
